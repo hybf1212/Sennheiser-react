555e1ea67186a23baff08bd629319c011da2fee5
hybf1212 <hybf1212@gmail.com>
2023-08-29 17:39:09 +0900

    부트스트랩과 부트스트랩 아이콘 연결, 상단 제작중

--- src/components/Hd.js
+++ src/components/Hd.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Search , Person , Cart } from 'react-bootstrap-icons';
 
 const Header = () => {
     return (
-        <div className='d-flex align-items-center'>
+        <div id='hd'>
+            <div className="wrap d-flex justify-content-between align-items-center">
 
+                <h1><a href=""><img src="" alt="top-logo" /></a></h1>
+                <ul className='d-flex'>
+                    <li>
+                        <a href="">Product</a>
+                        
+                    </li>
+                    <li><a href="">Support</a></li>
+                    <li><a href="">Downloads</a></li>
+                </ul>
+                <ul className='d-flex'>
+                    <li><a href=""><Search /></a></li>
+                    <li><a href=""><Person /></a></li>
+                    <li><a href=""><Cart /></a></li>
+                </ul>
+            </div>
         </div>
     )
 }
