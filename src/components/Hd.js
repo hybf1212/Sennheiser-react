959317196dfdbc6ed46d7273ba6ec8f91932f9b8
hybf1212 <hybf1212@gmail.com>
2023-09-06 15:07:42 +0900

    상단 이벤트 멘트 추가, 컨텐츠배너 완성

--- src/components/Hd.js
+++ src/components/Hd.js
@@ -47,53 +47,54 @@ const Header = () => {
         { searchv && <button  onClick={ ()=>{ searchvShow(false) }} className='text-white' style={{height:500}}>버튼</button> }
         
         <div id='hd' className='fixed-top'>
+            <div className={`hd-ad ${ isScrolled ? 'go-up' : '' }`}><a className='d-block text-center' href=""><span>회원가입 후 첫 구매시 10% 할인 코드 제공</span></a></div>
             
-                <div className={ `hdwrap d-flex justify-content-between align-items-center ${ isScrolled ? 'bg-transparent' : '' }` }>
-                    <h1><Link to='/'><img src={`${process.env.PUBLIC_URL}/img/Sennheiser_logo.svg`} alt="top-logo" /></Link></h1>
-                    <ul className='gnb d-flex'>
-                        <li className='product mx-3'>
-                            <a href="">Product<LuChevronDown className='ms-1'/></a>
-                            <ul className='lnb position-absolute d-flex justify-content-center align-items-center text-center'>
-                                <li className='lnb-menu'>
-                                    <div className='lnb-img'><a href=""><img src={`${process.env.PUBLIC_URL}/img/lnbimg/ie_600_product_shot.png`} alt="" /></a></div>
-                                    <a className='lnb-text' href="">이어폰</a>
-                                </li>                            
-                                <li className='lnb-menu'>
-                                    <div className='lnb-img'><a href=""><img src={`${process.env.PUBLIC_URL}/img/lnbimg/HD_660S2_ATF_Main_Image.png`} alt="" /></a></div>
-                                    <a className='lnb-text' href="">헤드폰</a>
-                                </li>
-                                <li className='lnb-menu'>
-                                    <div className='lnb-img'><a href=""><img src={`${process.env.PUBLIC_URL}/img/lnbimg/soundbar_plus_iso_front_dolby_atmos_final.png`} alt="" /></a></div>
-                                    <a className='lnb-text' href="">AMBEO</a>
-                                </li>
-                                <li className='lnb-menu'>
-                                    <div className='lnb-img'><a href=""><img src={`${process.env.PUBLIC_URL}/img/lnbimg/product_detail_x2_desktop_HDV-820_1_Sennheiser.png`} alt="" /></a></div>
-                                    <a className='lnb-text' href="">기타</a>
-                                </li>
-                                <li className='lnb-menu'>
-                                    <div className='lnb-img'><a href=""><img src={`${process.env.PUBLIC_URL}/img/lnbimg/product_detail_x2_desktop_Sennheiser_accessory_HD600_cable_and_adapter.png`} alt="" /></a></div>
-                                    <a className='lnb-text' href="">부품/ACC</a>
-                                </li>
+            <div className={ `hdwrap d-flex justify-content-between align-items-center ${ isScrolled ? 'bg-transparent' : '' }` }>
+                <h1><Link to='/'><img src={`${process.env.PUBLIC_URL}/img/Sennheiser_logo.svg`} alt="top-logo" /></Link></h1>
+                <ul className='gnb d-flex'>
+                    <li className='product mx-3'>
+                        <a href="">Product<LuChevronDown className='ms-1'/></a>
+                        <ul className='lnb position-absolute d-flex justify-content-center align-items-center text-center'>
+                            <li className='lnb-menu'>
+                                <div className='lnb-img'><a href=""><img src={`${process.env.PUBLIC_URL}/img/lnbimg/ie_600_product_shot.png`} alt="" /></a></div>
+                                <a className='lnb-text' href="">이어폰</a>
+                            </li>                            
+                            <li className='lnb-menu'>
+                                <div className='lnb-img'><a href=""><img src={`${process.env.PUBLIC_URL}/img/lnbimg/HD_660S2_ATF_Main_Image.png`} alt="" /></a></div>
+                                <a className='lnb-text' href="">헤드폰</a>
+                            </li>
+                            <li className='lnb-menu'>
+                                <div className='lnb-img'><a href=""><img src={`${process.env.PUBLIC_URL}/img/lnbimg/soundbar_plus_iso_front_dolby_atmos_final.png`} alt="" /></a></div>
+                                <a className='lnb-text' href="">AMBEO</a>
+                            </li>
+                            <li className='lnb-menu'>
+                                <div className='lnb-img'><a href=""><img src={`${process.env.PUBLIC_URL}/img/lnbimg/product_detail_x2_desktop_HDV-820_1_Sennheiser.png`} alt="" /></a></div>
+                                <a className='lnb-text' href="">기타</a>
+                            </li>
+                            <li className='lnb-menu'>
+                                <div className='lnb-img'><a href=""><img src={`${process.env.PUBLIC_URL}/img/lnbimg/product_detail_x2_desktop_Sennheiser_accessory_HD600_cable_and_adapter.png`} alt="" /></a></div>
+                                <a className='lnb-text' href="">부품/ACC</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li className='mx-3'><a href="">Support</a></li>
+                    <li className='mx-3'><a href="">Downloads</a></li>
+                </ul>
+                <ul className='side-nav d-flex align-items-center'>
+                    <li>
+                        <div className='language position-relative'>
+                            <button onClick={ ()=>{ languageSet('kr')}}>KR<LuChevronDown/></button>
+                            <ul className='position-absolute'>
+                                <li><button onClick={ ()=>{ languageSet('jp')}}>JP</button></li>
+                                <li><button onClick={ ()=>{ languageSet('eng')}}>ENG</button></li>
                             </ul>
-                        </li>
-                        <li className='mx-3'><a href="">Support</a></li>
-                        <li className='mx-3'><a href="">Downloads</a></li>
-                    </ul>
-                    <ul className='side-nav d-flex align-items-center'>
-                        <li>
-                            <div className='language position-relative'>
-                                <button onClick={ ()=>{ languageSet('kr')}}>KR<LuChevronDown/></button>
-                                <ul className='position-absolute'>
-                                    <li><button onClick={ ()=>{ languageSet('jp')}}>JP</button></li>
-                                    <li><button onClick={ ()=>{ languageSet('eng')}}>ENG</button></li>
-                                </ul>
-                            </div>
-                        </li>                        
-                        <li><button onClick={ ()=>{ searchvShow(true)}} ><img src={sicon} /></button></li>
-                        <li><Link to='/user'><img src={uicon} /></Link></li>
-                        <li><Link to='/cart'><img src={cicon} /></Link></li>
-                    </ul>
-                </div>
+                        </div>
+                    </li>                        
+                    <li><button onClick={ ()=>{ searchvShow(true)}} ><img src={sicon} /></button></li>
+                    <li><Link to='/user'><img src={uicon} /></Link></li>
+                    <li><Link to='/cart'><img src={cicon} /></Link></li>
+                </ul>
+            </div>
                     
         </div>
         </>
--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,16 @@ import React from 'react'
 const Footer = () => {
     return(
         <div id='footer'>
-            
+            <div className='d-flex justify-content-between'>
+                <div>
+                    <span>이민호의 Sennheiser 광고웹앱 포트폴리오</span>
+                    <span className='ms-4'>TEL / 010 - 8901 - 2841</span>
+                </div>
+                <div>
+                    <a href=""></a>
+                </div>        
+            </div>
+            <div></div>
         </div>
     )
 }
